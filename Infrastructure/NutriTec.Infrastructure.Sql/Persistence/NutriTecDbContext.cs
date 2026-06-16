48dfc8e26b8eadec4b359bf1ae26b5133b3c1e26
Mario <mario28.ce@gmail.com>
2026-06-15 23:17:00 -0600

    Ajuste Tests

--- Infrastructure/NutriTec.Infrastructure.Sql/Persistence/NutriTecDbContext.cs
+++ Infrastructure/NutriTec.Infrastructure.Sql/Persistence/NutriTecDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using NutriTec.Domain.Productos;
 using NutriTec.Infrastructure.Sql.Persistence.Entities;
 
 namespace NutriTec.Infrastructure.Sql.Persistence;
@@ -7,6 +8,7 @@ public sealed class NutriTecDbContext(DbContextOptions<NutriTecDbContext> option
 {
     public DbSet<AdministradorSql> Administradores => Set<AdministradorSql>();
     public DbSet<TipoCobroSql> TiposCobro => Set<TipoCobroSql>();
+    public DbSet<Producto> Productos => Set<Producto>();
     public DbSet<UsuarioSql> Usuarios => Set<UsuarioSql>();
     public DbSet<NutricionistaSql> Nutricionistas => Set<NutricionistaSql>();
 
