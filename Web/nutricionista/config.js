dd4d2ae1ca574784ea9619ab55e0b290e6454a44
Mario <mario28.ce@gmail.com>
2026-06-23 14:45:33 -0600

    hdhg

--- Web/nutricionista/config.js
+++ Web/nutricionista/config.js
@@ -1,5 +1,5 @@
 window.NUTRITEC_CONFIG = {
-    USE_MOCKS: false,
-    SQL_API_BASE_URL: "https://app-nutritec-sql-api-demo.azurewebsites.net/api",
-    MONGO_API_BASE_URL: "https://app-nutritec-mongo-api-demo.azurewebsites.net/api",
+  USE_MOCKS: false,
+  SQL_API_BASE_URL: "https://app-nutritec-sql-api-demo.azurewebsites.net/api",
+  MONGO_API_BASE_URL: "https://app-nutritec-mongo-api-demo.azurewebsites.net/api",
 };
