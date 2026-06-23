49c9bb138202c767b919f6ca9dc4dacd87e36d2b
Mario <mario28.ce@gmail.com>
2026-06-23 12:32:09 -0600

    algunas cosas mas

--- Vista Cliente/frontend/config.js
+++ Vista Cliente/frontend/config.js
@@ -1,7 +1,5 @@
-// Configuración runtime para Azure Static Web Apps.
-// Cambia estas URLs por las URLs públicas de tus APIs en Azure App Service.
 window.NUTRITEC_CONFIG = {
   USE_MOCKS: false,
-  SQL_API_BASE_URL: "/api/sql",
-  MONGO_API_BASE_URL: "/api/mongo",
+  SQL_API_BASE_URL: "https://app-nutritec-sql-api-demo.azurewebsites.net/api",
+  MONGO_API_BASE_URL: "https://app-nutritec-mongo-api-demo.azurewebsites.net/api",
 };
